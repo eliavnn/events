dd8353f75fa94e676f292ef2f6c61d327fc50242
0xelia <elia.vanon@yahoo.com>
2024-10-04 22:39:58 +0200

    fulll dashed button

--- resources/js/Components/DashedAddButton.vue
+++ resources/js/Components/DashedAddButton.vue
@@ -2,7 +2,7 @@
     <button
     @click="emits('add-clicked')"
     :class="[width]"
-    class="aspect-[5/3] border-2 border-dashed border-gray-400 rounded-lg">
+    class="aspect-[5/3] border-2 border-dashed border-gray-300 rounded-lg">
 
     <i class="pi pi-plus text-xl"></i>
 
