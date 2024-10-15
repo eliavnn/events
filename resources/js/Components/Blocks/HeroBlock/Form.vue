439ff282db67af3115ab3bcba63fe85674c1136f
0xelia <elia.vanon@yahoo.com>
2024-10-15 23:38:25 +0200

    forms

---
+++ resources/js/Components/Blocks/HeroBlock/Form.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h3 class="text-3xl">
+            {{ context.type }}
+        </h3>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    context: Object
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- resources/js/Pages/Events/Create.vue
+++ resources/js/Pages/Events/Create.vue
@@ -1,13 +1,27 @@
 <template>
     <div>
-        {{ blocks }}
+        <component is="Form"  />
     </div>
 </template>
 
 <script setup>
 
+import { onMounted } from "vue";
+
 const props = defineProps({
     blocks: Object
+})
+
+Object.entries(props.blocks).forEach(([key, value]) => {
+    console.log(value.type, value)
+})
+
+function resolveComponent(type){
+
+}
+
+onMounted(async () => {
+    await console.log(import("@/Components/Blocks/HeroBlock/Form.vue").then(c => c))
 })
 
 </script>
